86203dd659e9f5ee962e27c930de417431ab09ad
Andrea Franchini <f.andrea1602@icloud.com>
2014-08-28 17:04:04 +0200

    f

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,16 @@
-//--make an animates canvas
+$('header, section').css("height", window.innerHeight);
+$('header, section, footer').css("width", window.innerWidth);
+
+var lastScrollTop = 0;
+
+$(window).scroll(function(event){
+   var scrollTopNow = $(this).scrollTop();
+   if (scrollTopNow > lastScrollTop){
+       //scroll down
+       
+   } else {
+	   //scroll up
+	  
+   }
+   lastScrollTop = st;
+});
