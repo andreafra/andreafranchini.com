b0e29fd3e39e58b3c5c4b37b91eb2fc3f9b29e48
Andrea Franchini <f.andrea1602@icloud.com>
2014-08-30 11:22:14 +0200

    minor update

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,7 @@ $('header, section, footer').css("width", window.innerWidth);
 
 var lastScrollTop = 0;
 
-$(window).scroll(function(event){
-   var scrollTopNow = $(this).scrollTop();
-   if (scrollTopNow > lastScrollTop){
-       //scroll down
-       
-   } else {
-	   //scroll up
-	  
-   }
-   lastScrollTop = st;
-});
+var scrollToDiv = function(target){
+	$('html, body').animate({
+	    scrollTop: $(target).offset().top
+	}, 1000);}
