ea1a1993fdde3cf96a93d260e44b059e3f183a5d
Andrea Franchini <f.andrea1602@icloud.com>
2014-09-26 21:07:16 +0200

    AngularJS flavor added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,12 @@
 (function(){
 	var app = angular.module('view', [ ]);
-
 	app.controller("PanelController", function(){
   		this.tab = 1;
 		this.selectTab = function(setTab) {
-			if(setTab===true) {
-				this.tab = tab - 1;
-				console.log(tab);
-			} else {
-				this.tab = tab + 1;
-				console.log(tab);
-			}
+				this.tab = setTab;
 		};
 		this.isSelected = function(checkTab){
 			return this.tab === checkTab;
 		};
-
 	});
-
-}) ();
-
-$('#greets').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-	$('#title-1').addClass('bounceInDown').removeClass('hidden');
-	$('#title-1').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-		$('#title-2').addClass('fadeIn').removeClass('hidden');
-		$('#title-2').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-			$('.left-button, .right-button').addClass('fadeIn').removeClass('hidden');
-			
-		});
-	});
-});
-//tooltip
-$('.more-info').tooltip();
+})();
