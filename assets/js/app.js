d1f44c227a647f0d7ce1d12651eef40eef3c7b1b
Andrea Franchini <f.andrea1602@icloud.com>
2014-09-26 19:31:45 +0200

    bugs?

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,30 @@
-// var pages = 3;
-// var page = 0;
-// $('.left-button').click(function(){
-// 	if(page === 0) {
-// 		page = pages;
-// 				console.log(page);
-// 	} else {
-// 		page = page - 1;
-// 				console.log(page);
+(function(){
+	var app = angular.module('view', [ ]);
 
-// 	}
-// });
-// $('.right-button-button').click(function(){
-// 	if(page === 0) {
-// 		page = pages;
-// 				console.log(page);
+	app.controller("PanelController", function(){
+  		this.tab = 1;
+		this.selectTab = function(setTab) {
+			if(setTab===true) {
+				this.tab = tab - 1;
+				console.log(tab);
+			} else {
+				this.tab = tab + 1;
+				console.log(tab);
+			}
+		};
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab;
+		};
 
-// 	} else {
-// 		page = page + 1;
-// 				console.log(page);
+	});
 
-// 	}
-// });
-//animations
+}) ();
+
 $('#greets').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-	$('#title-desc-1').addClass('bounceInDown').removeClass('hidden');
-	$('#title-desc-1').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-		$('#title-desc-2').addClass('fadeIn').removeClass('hidden');
-		$('#title-desc-2').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+	$('#title-1').addClass('bounceInDown').removeClass('hidden');
+	$('#title-1').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+		$('#title-2').addClass('fadeIn').removeClass('hidden');
+		$('#title-2').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
 			$('.left-button, .right-button').addClass('fadeIn').removeClass('hidden');
 			
 		});
