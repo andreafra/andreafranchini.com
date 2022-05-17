c85e072317cea09aa6329f58b394257f2622b874
Andrea Franchini <hello@andreafranchini.com>
2022-05-18 00:43:34 +0200

    Create Clean Slate

--- README.md
+++
@@ -1,6 +0,0 @@
-andreafranchini.com
-===============
-
-This is the source code of my website.
-
-Its last iteration has been built with [GatsbyJS](https://www.gatsbyjs.com/).
--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-// normalize CSS
-import "./src/css/normalize.css"
-
-// custom CSS styles
-import "./src/css/global.css"
-
-// Highlighting for code blocks
-import "prismjs/themes/prism.css"
