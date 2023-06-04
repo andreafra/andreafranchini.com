cf2a0b4b737b53e1aa4a3e843c2bf2de4f69ff80
Andrea Franchini <hello@andreafranchini.com>
2023-06-04 12:28:41 +0200

    small update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,13 @@
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-import { astroImageTools } from "astro-imagetools";
+import { defineConfig } from "astro/config"
+import sitemap from "@astrojs/sitemap"
+import { astroImageTools } from "astro-imagetools"
 
-import mdx from "@astrojs/mdx";
+import mdx from "@astrojs/mdx"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), astroImageTools, mdx()],
-  // Deploy
-  site: "https://www.andreafranchini.com",
-  // Enable experimental integrations
-  experimental: {
-    integrations: true,
-    assets: true
-  }
-});
+	integrations: [sitemap(), astroImageTools, mdx()],
+	compressHTML: true,
+	// Deploy
+	site: "https://andreafranchini.com",
+})
